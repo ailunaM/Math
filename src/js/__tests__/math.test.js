dca1f0d8b33798ecec8eb599c39a1bcf57233b12
ailunaM <ajlunaravsanbekova@gmail.com>
2025-01-21 04:25:19 +0600

    task№1 done with test

--- src/js/__tests__/math.test.js
+++ src/js/__tests__/math.test.js
@@ -1,1 +1,30 @@
-test("", () => {});
+// test("", () => {});
+import {  Magician, Daemon } from "../math";
+
+test("Attack without stoned", () => {
+  const magician = new Magician(100);
+
+  const daemon = new Daemon(100);
+
+  expect(magician.getAttack(1)).toBe(100);
+  expect(magician.getAttack(2)).toBe(90);
+  expect(magician.getAttack(3)).toBe(80);
+  expect(magician.getAttack(4)).toBe(70);
+  expect(magician.getAttack(5)).toBe(60);
+
+  expect(daemon.getAttack(1)).toBe(100);
+  expect(daemon.getAttack(2)).toBe(90);
+  expect(daemon.getAttack(3)).toBe(80);
+  expect(daemon.getAttack(4)).toBe(70);
+  expect(daemon.getAttack(5)).toBe(60);
+});
+test('Attack with distance greater than 5', () => {
+    const magician = new Magician(100);
+    const daemon = new Daemon(100);
+
+    magician.stoned = true;
+    daemon.stoned = true;
+
+    expect(magician.getAttack(10)).toBe(0);  
+    expect(daemon.getAttack(10)).toBe(0);
+});
--- src/js/math.js
+++ src/js/math.js
@@ -1,1 +1,65 @@
+export class Character {
+  constructor(baseAttack) {
+    this.baseAttack = baseAttack;
+    this._stoned = false;
 
+    this.buffer = new ArrayBuffer(4);
+    this.view = new DataView(this.buffer);
+  }
+
+  get stoned() {
+    return this._stoned;
+  }
+
+  set stoned(value) {
+    this._stoned = value;
+  }
+
+  getAttack(distance) {
+    let attack;
+
+    switch (distance) {
+      case 1:
+        attack = this.baseAttack;
+        break;
+      case 2:
+        attack = this.baseAttack * 0.9;
+        break;
+      case 3:
+        attack = this.baseAttack * 0.8;
+        break;
+      case 4:
+        attack = this.baseAttack * 0.7;
+        break;
+      case 5:
+        attack = this.baseAttack * 0.6;
+        break;
+      default:
+        attack = 0;
+    }
+
+    if (this._stoned) {
+      attack -= Math.log2(distance) * 5;
+    }
+
+    this.view.setFloat32(0, Math.max(attack, 0));
+
+    return Math.max(attack, 0);
+  }
+}
+
+export class Magician extends Character {
+  constructor(baseAttack) {
+    super(baseAttack);
+  }
+}
+
+export class Daemon extends Character {
+  constructor(baseAttack) {
+    super(baseAttack);
+  }
+}
+const magician = new Magician(100);
+const daemon = new Daemon(100);
+console.log("Magician attack on distance 2:", magician.getAttack(2));  
+console.log("Daemon attack on distance 3:", daemon.getAttack(3));
